a58e6e458b9c6a73eeaab30896ce933906947c22
eladfal <eladfal@ac6379a7-0c51-0410-9bd9-d57a86200d78>
2008-09-20 16:29:53 +0000

    Added the web search functionality and started working on the cache part

---
+++ trunk/Model/CacheSearch.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Runtime.InteropServices;
+
+namespace Model
+{
+    public class CacheSearch
+    {
+        #region CacheSearch : Members & Consts
+
+            private const int ERROR_FILE_NOT_FOUND = 2;
+
+        #endregion
+
+        #region CacheSearch : Properties
+
+        #endregion
+
+        #region CacheSearch : Events
+
+        #endregion
+
+        #region CacheSearch : Event Handlers
+
+        #endregion
+
+        #region CacheSearch : Methods
+
+            [DllImport("Wininet.dll", SetLastError = true, CharSet = CharSet.Auto)]
+            public static extern Boolean GetUrlCacheEntryInfo(String lpxaUrlName, IntPtr lpCacheEntryInfo, ref int lpdwCacheEntryInfoBufferSize);
+
+            public static string GetPathForCachedFile(string fileUrl)
+            {
+                int cacheEntryInfoBufferSize = 0;
+                IntPtr cacheEntryInfoBuffer = IntPtr.Zero;
+                int lastError; Boolean result;
+                try
+                {
+                    // call to see how big the buffer needs to be
+                    result = GetUrlCacheEntryInfo(fileUrl, IntPtr.Zero, ref cacheEntryInfoBufferSize);
+                    lastError = Marshal.GetLastWin32Error();
+                    if (result == false)
+                    {
+                        if (lastError == ERROR_FILE_NOT_FOUND) return null;
+                    }
+                    // allocate the necessary amount of memory
+                    cacheEntryInfoBuffer = Marshal.AllocHGlobal(cacheEntryInfoBufferSize);
+
+                    // make call again with properly sized buffer
+                    result = GetUrlCacheEntryInfo(fileUrl, cacheEntryInfoBuffer, ref cacheEntryInfoBufferSize);
+                    lastError = Marshal.GetLastWin32Error();
+                    if (result == true)
+                    {
+                        Object strObj = Marshal.PtrToStructure(cacheEntryInfoBuffer, typeof(CacheEntryInfo));
+                        CacheEntryInfo internetCacheEntry = (CacheEntryInfo)strObj;
+                        String localFileName = Marshal.PtrToStringAuto(internetCacheEntry.lpszLocalFileName); return localFileName;
+                    }
+                    else return null;// file not found
+                }
+                finally
+                {
+                    if (!cacheEntryInfoBuffer.Equals(IntPtr.Zero)) Marshal.FreeHGlobal(cacheEntryInfoBuffer);
+                }
+            }    
+
+        #endregion
+       
+    }
+}
---
+++ trunk/Model/ModelGoogleSearchResult.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Model
+{
+    public class ModelGoogleSearchResult : ModelItem
+    {
+        #region ModelGoogleSearchResult : Members & Consts
+            
+            private string url;
+            private string title;
+            private string content;
+
+        #endregion
+
+        #region ModelGoogleSearchResult : Initialization
+            
+            public ModelGoogleSearchResult(string url, string title, string content)
+            {
+                this.url = url;
+                this.title = title;
+                this.content = content;
+            }
+
+        #endregion
+
+        #region ModelGoogleSearchResult : Properties
+
+            public string URL
+            {
+                get
+                {
+                    return this.url;
+                }
+            }
+
+            public string Title
+            {
+                get
+                {
+                    return this.title;
+                }
+            }
+
+            public string Content
+            {
+                get
+                {
+                    return this.content;
+                }
+            }
+
+        #endregion
+
+        #region ModelGoogleSearchResult : Events
+
+        #endregion
+
+        #region ModelGoogleSearchResult : Event Handlers
+
+        #endregion
+
+        #region ModelGoogleSearchResult : Methods
+
+        #endregion
+    }
+}
